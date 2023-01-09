8be38106c9bc7d4ccdec581975ed289dd23a9cff
Muhammad Awaludin <am.awaludinmuhammad@gmail.com>
2023-01-09 12:54:27 +0700

    add table migrations

---
+++ migrations/20230109055059_add_slug_to_brands_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.table('brands', (table) => {
+    table.string('slug');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.table('brands', (table) => {
+    table.dropColumn('slug');
+  });
+};
